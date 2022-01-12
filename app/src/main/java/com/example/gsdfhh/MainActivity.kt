1f495c7c38ada1d457a15445c183f4205af85fd8
matipoklo <poklojr@gmail.com>
2022-01-12 11:01:00 +0100

    nic nie dziala, zacznij od nowa

--- app/src/main/java/com/example/gsdfhh/MainActivity.kt
+++ app/src/main/java/com/example/gsdfhh/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.gsdfhh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btndalej).setOnClickListener(){
+            findViewById<ImageView>(R.id.imgGaleria).setImageResource(R.drawable.fabianski)
+        }
+        findViewById<Button>(R.id.btndalej).setOnClickListener(){
+            findViewById<ImageView>(R.id.imgGaleria).setImageResource(R.drawable.szczesny)
+        }
+        findViewById<Button>(R.id.btndalej).setOnClickListener(){
+            findViewById<ImageView>(R.id.imgGaleria).setImageResource(R.drawable.schmeihel)
+        }
     }
+
 }
